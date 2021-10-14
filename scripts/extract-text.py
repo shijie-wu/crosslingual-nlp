9863200fd062c93c9b11f4d0b2bbd355af539b35
shabnam-b <shabnam.behzad7@gmail.com>
2021-10-14 15:41:10 -0400

    update scripts

--- scripts/extract-text.py
+++ scripts/extract-text.py
@@ -4,7 +4,14 @@ from typing import Dict, Type
 import fire
 
 sys.path.append("src")
-from dataset import Dataset, ParsingDataset, WikiAnnNER  # noqa: E402
+from dataset import (  # noqa: E402
+    ACEDataset,
+    BetterDataset,
+    Dataset,
+    MUCDataset,
+    ParsingDataset,
+    WikiAnnNER,
+)
 
 
 def main(task: str, path: str, lang: str, split: str):
@@ -12,6 +19,9 @@ def main(task: str, path: str, lang: str, split: str):
     MAPPING: Dict[str, Type[Dataset]] = {
         "wikiann": WikiAnnNER,
         "ud": ParsingDataset,
+        "better-abstract": BetterDataset,
+        "ace": ACEDataset,
+        "muc": MUCDataset,
     }
     assert task in MAPPING
     CLASS = MAPPING[task]
@@ -21,7 +31,13 @@ def main(task: str, path: str, lang: str, split: str):
         print("Empty file path")
         exit()
     for example in CLASS.read_file(file_path, lang, split):
-        if task == "ud" or task == "wikiann":
+        if task == "ace":
+            for s in example["sentences"]:
+                print(" ".join(s["tokens"]))
+        elif task == "muc":
+            for i in example["sent"]:
+                print(" ".join(i))
+        else:
             print(" ".join(example["sent"]))
 
 
